601d5fab39987ab04215967b74d97d0b1686fde7
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2022-09-09 09:13:15 +0200

    creation des props

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+
+    import bed from "../components/icons/bed.vue";
+    import bath from "../components/icons/bath.vue";
+    import mcarre from "../components/icons/mcarre.vue";
+
+    const proprietes = defineProps({
+      name : {type: String, default:'Tarpon Bat'                                },
+      price: {type:Number, default:'4800'                                        },
+      adresse: {type: String, default: '103 Lake Shores, Michigan, IN'          },
+      bed: {type:Number , default: 4                                            },
+      bathroom: {type:Number , default: 2                                       },
+      mcarre: {type:String , default: '6x7.5'                                    },
+      });
+    
+     
+</script>
+
 <template>
      <figure class="flex flex-col mt-12 mx-8">
     <img
@@ -20,8 +38,8 @@
       <div class="flex justify-between">
         <div class="w-2/5">
           <div class="flex items-center">
-            <p class="text-2xl font-bold text-indigo-500">$4800</p>
-            <span class="text-xs font-normal text-gray-900">/month</span>
+            <p class="text-2xl font-bold text-indigo-500"></p>
+            <span class="text-xs font-normal text-gray-900">{{proprietes.price}} /month</span>
           </div>
           <p class="text-2xl font-bold">Tarpon Bay</p>
         </div>
@@ -40,21 +58,21 @@
         </div>
       </div>
       <p class="text-base font-normal text-gray-900">
-        103 Lake Shores, Michigan, IN
+        {{proprietes.adresse}}
       </p>
       <div class="w-full border border-indigo-50"></div>
       <div class="flex justify-between">
         <div class="flex items-center gap-2">
           <bed class="h-5 stroke-indigo-500"></bed>
-          <span class="text-xs text-gray-500">4 Beds</span>
+          <span class="text-xs text-gray-500">{{proprietes.bed}} Beds</span>
         </div>
         <div class="flex items-center gap-2">
           <bath class="h-5 stroke-indigo-500"></bath>
-          <span class="text-xs text-gray-500">2 Bathrooms</span>
+          <span class="text-xs text-gray-500">{{proprietes.bath}} Bathrooms</span>
         </div>
         <div class="flex items-center gap-2">
           <mcarre class="h-5 stroke-indigo-500"></mcarre>
-          <span class="text-xs text-gray-500">6x7.5 m²</span>
+          <span class="text-xs text-gray-500">{{proprietes.mcarre}} m²</span>
         </div>
       </div>
     </figcaption>
@@ -63,18 +81,3 @@
   
 </template>
 
-<script>
-import bed from "../components/icons/bed.vue";
-import bath from "../components/icons/bath.vue";
-import mcarre from "../components/icons/mcarre.vue";
-
-export default {
-  name: "App",
-  components: {
-    bed,
-    bath,
-    mcarre,
-  },
-}
-
-</script>
--- src/pages/Simple.vue
+++ src/pages/Simple.vue
@@ -1,12 +1,29 @@
 <script setup lang="ts">
 import card from "../components/card.vue";
-   
+
+ const proprietes = defineProps({
+  name : {type: String, default:'Tarpon Bat'    },
+  prix: {type:Number, default:'4800'      },
+  adresse: {type: String, default: '103 Lake Shores, Michigan, IN'       },
+  bed: {type:Number , default: 4           },
+  bathroom: {type:Number , default: 2           },
+  mcarr: {type:String , default: '6x7.5'           },
+  
+    });
+
+  /*  prix: Number,   
+    nom: String,
+    adresse: String,
+    bed: String,
+    bathroom: String,
+    mcarre: Number,
+*/
+
 </script>
 
 <template>
     <div>
         <p>Simple</p>
-        
         <card></card>
     </div>
 </template>
